09e289c4f05596142f88b0cb85f64429abf16faa
Kaiser19 <ubuntu@ip-172-31-80-7.ec2.internal>
2019-10-16 06:07:24 +0000

    Added Database

---
+++ db/migrate/20191016060524_create_passes.rb
@@ -0,0 +1,12 @@
+class CreatePasses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :passes do |t|
+      t.string :from
+      t.string :code
+      t.datetime :expiry
+      t.integer :code_id
+
+      t.timestamps
+    end
+  end
+end
