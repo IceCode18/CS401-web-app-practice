3cb63f43f829931a57000a36c6949c106f657695
Kaiser19 <ubuntu@ip-172-31-80-7.ec2.internal>
2019-10-17 07:22:08 +0000

    Everything working

--- app/controllers/passes_controller.rb
+++ app/controllers/passes_controller.rb
@@ -1,4 +1,7 @@
 class PassesController < ApplicationController
+  before_action :load_pass, except: [:index, :new, :create]
+  
+  
   def index
    @passes = Pass.all
   end
@@ -17,15 +20,12 @@ class PassesController < ApplicationController
   end
   
   def show
-    @pass = Pass.find params[:id]
   end
   
   def edit
-    @pass = Pass.find params[:id]
   end
   
   def update
-    @pass = Pass.find params[:id]
     if @pass.update pass_params
       redirect_to @pass, notice: "Guest Pass Updated!"
     else
@@ -34,12 +34,15 @@ class PassesController < ApplicationController
   end
   
   def destroy
-    @pass = Pass.find params[:id]
     @pass.destroy
     redirect_to passes_path, alert: "Guest Pass Deleted."
   end
   
   private
+  
+  def load_pass
+    @pass = Pass.find params[:id]
+  end
   
   def pass_params
     params.require(:pass).permit(:from,:code_id,:expiry,:code)
