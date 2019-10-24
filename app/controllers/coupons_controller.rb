6718f9d9086b981867ee4fe88886502cea6ce47b
Kaiser19 <ubuntu@ip-172-31-80-7.ec2.internal>
2019-10-24 08:44:18 +0000

    Added Show, Edit functions

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -9,6 +9,19 @@ class CouponsController < ApplicationController
     @coupon = Coupon.new
   end  
   
+  def create
+    @coupon = Coupon.new coupon_params
+    if @pass.coupons << @coupon
+      redirect_to pass_coupons_path(@pass), notice: "Coupon Saved!"
+    else
+      render :new
+    end
+  end
+  
+  def show
+    @coupon = @pass.coupons.find params[:id]
+  end  
+    
   
   private
   
@@ -16,6 +29,8 @@ class CouponsController < ApplicationController
     @pass = Pass.find params[:pass_id]
   end
   
-
+  def coupon_params
+    params.require(:coupon).permit(:code, :code_expiry, :note, :claimer)
+  end
   
 end
--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,6 +1,5 @@
 class Coupon < ApplicationRecord
     belongs_to :pass, foreign_key: "code_id"
-    validates :code_id, presence: true
     validates :code_expiry, presence: true
     validates :code, presence: true
 end
