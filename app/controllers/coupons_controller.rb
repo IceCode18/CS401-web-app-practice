0849cd6176d1fd09a6dbb6bb3133edba66ee70e8
Kaiser19 <ubuntu@ip-172-31-80-7.ec2.internal>
2019-10-24 06:55:32 +0000

    Added Coupon Interface

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,4 +1,15 @@
 class CouponsController < ApplicationController
+  before_action :load_pass
+  
   def index
+    @coupons = @pass.coupons
   end
+  
+  private
+  
+  def load_pass
+    @pass = Pass.find params[:pass_id]
+  end
+  
+  
 end
