74d28b8373cecd552134f0ab595958e89a5ac6be
Kaiser19 <ubuntu@ip-172-31-80-7.ec2.internal>
2019-11-05 21:47:11 +0000

    User Database Migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_052635) do
+ActiveRecord::Schema.define(version: 2019_11_05_214500) do
 
   create_table "coupons", force: :cascade do |t|
     t.string "code"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 2019_10_24_052635) do
     t.string "code"
     t.datetime "expiry"
     t.integer "code_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
