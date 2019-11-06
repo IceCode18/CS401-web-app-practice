7d1ee0dd9d2ac5af18cf9eafa9cf6ada74d36ec4
Kaiser19 <ubuntu@ip-172-31-80-7.ec2.internal>
2019-11-06 02:00:49 +0000

    Edited Routes F

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  #get 'users/index'
   # get 'coupons/index'
+  
+  resources :users
   resources :passes do
     resources :coupons
   end
+  
+  
   root to: 'home#index'
+  get "about" => 'home#about'
   get "claim" => 'home#claim'
   get "feedback" => 'home#feedback'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
