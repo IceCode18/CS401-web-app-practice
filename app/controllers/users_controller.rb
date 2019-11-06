218cfa6023355828ddf60542c43e0cf051e721ea
Kaiser19 <ubuntu@ip-172-31-80-7.ec2.internal>
2019-11-06 02:58:07 +0000

    Centralized Login and Logout in Applications Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      login(@user)
       redirect_to root_path, notice: "Acoount Created."
     else
       render :new
     end
   end
   
+
   
   private
   
