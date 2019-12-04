383bbb4e97c777c552609c2e2ea70f1762acc293
Kaiser19 <ubuntu@ip-172-31-80-7.ec2.internal>
2019-12-04 23:52:12 +0000

    Updated link for Claim Page

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   
   root to: 'home#index'
   get "about" => 'home#about'
-  get "claim" => 'home#claim'
+  get "claim" => 'public_coupons#index'
   get "feedback" => 'home#feedback'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
